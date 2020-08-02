5e2bb9988d64a284501a8df1cf72e4994764b777
Awhit-commits <ndrw_whtmr@ymail.com>
2020-08-02 14:52:48 -0500

    commented out line 22 in index.js

--- server/app.js
+++ server/app.js
@@ -12,14 +12,14 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
